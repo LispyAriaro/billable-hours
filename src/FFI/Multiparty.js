cc0cd07295b6c7c1f54a41aa5c4dfd936f01bb27
Efe Ariaroo <efeariaroo@gmail.com>
2019-07-26 23:47:02 +0100

    can now return sensible json response

--- src/FFI/Multiparty.js
+++ src/FFI/Multiparty.js
@@ -5,30 +5,32 @@ var fs = require('fs');
 
 exports.grabUploadData = function (req) {
   return function (fieldName) {
-    return new Promise(function(resolve, reject) {
-      try {
-        var form = new multiparty.Form();
+    return function() {
+      return new Promise(function(resolve, reject) {
+        try {
+          var form = new multiparty.Form();
 
-        form.parse(req, function(err, fields, files) {
-          if(fields) {
-            if(files && files[fieldName] && !fs.lstatSync(files[fieldName][0].path).isDirectory()) {
-              console.log("files[fieldName][0]: ", files[fieldName][0]);
+          form.parse(req, function(err, fields, files) {
+            if(fields) {
+              if(files && files[fieldName] && !fs.lstatSync(files[fieldName][0].path).isDirectory()) {
+                console.log("files[fieldName][0]: ", files[fieldName][0]);
 
-              fs.readFile(files[fieldName][0].path, 'utf8', function(err, contents) {
-                resolve(contents);
-              });
-            } else {
-              reject("Sorry, you did not select any file!");
+                fs.readFile(files[fieldName][0].path, 'utf8', function(err, contents) {
+                  resolve(contents);
+                });
+              } else {
+                reject("Sorry, you did not select any file!");
+              }
             }
-          }
-        });
-      } catch (e) {
-        var errorMsg = "Sorry, uploading the file failed!";
-        reject(errorMsg);
-      }
-    }).catch(function() {
-      var errorMsg = "Sorry, uploading the image failed 2!";
-      return {status: 'failed', error: errorMsg};
-    });
+          });
+        } catch (e) {
+          var errorMsg = "Sorry, uploading the file failed!";
+          reject(errorMsg);
+        }
+      }).catch(function() {
+        var errorMsg = "Sorry, uploading the image failed 2!";
+        return {status: 'failed', error: errorMsg};
+      });
+    }
   }
 }
